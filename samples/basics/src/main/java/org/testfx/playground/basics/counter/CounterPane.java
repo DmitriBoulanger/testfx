2185a8a1ae52ef2d893f4e1e42cef41365511f03
Dmitri Boulanger <Dmitri Boulanger@Hombach-PC>
2016-03-20 16:11:53 +0100

    a bit refactored

---
+++ samples/basics/src/main/java/org/testfx/playground/basics/counter/CounterPane.java
@@ -0,0 +1,43 @@
+package org.testfx.playground.basics.counter;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+
+public class CounterPane extends StackPane {
+    
+    private static int asInteger(String value) { 
+	return Integer.parseInt(value); 
+    }
+    private static String asString(int value) { 
+	return value + ""; 
+    }
+
+    public CounterPane() {
+        // create countButton.
+        Button countButton = new Button("count");
+        countButton.setId("countButton");
+
+        // create countValue.
+        TextField countValue = new TextField("0");
+        countValue.setId("countValue");
+
+        // initialize controls.
+        countButton.setOnAction(event -> {
+            int value = asInteger(countValue.getText());
+            int incrementedValue = value + 1;
+            countValue.setText(asString(incrementedValue));
+        });
+        countValue.setEditable(false);
+        countValue.setPrefWidth(50);
+
+        // create and add containers.
+        HBox box = new HBox(10, countButton, countValue);
+        box.setPadding(new Insets(10));
+        box.setAlignment(Pos.CENTER);
+        getChildren().add(box);
+    }
+}
--- samples/basics/src/test/java/org/testfx/playground/basics/counter/CounterTest.java
+++ samples/basics/src/test/java/org/testfx/playground/basics/counter/CounterTest.java
@@ -1,51 +1,15 @@
 package org.testfx.playground.basics.counter;
 
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.service.query.impl.NodeQueryUtils.hasText;
 
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 
-import static org.testfx.api.FxAssert.verifyThat;
-import static org.testfx.service.query.impl.NodeQueryUtils.hasText;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class CounterTest extends ApplicationTest {
-
-    private static class CounterPane extends StackPane {
-        public CounterPane() {
-            // create countButton.
-            Button countButton = new Button("count");
-            countButton.setId("countButton");
-
-            // create countValue.
-            TextField countValue = new TextField("0");
-            countValue.setId("countValue");
-
-            // initialize controls.
-            countButton.setOnAction(event -> {
-                int value = asInteger(countValue.getText());
-                int incrementedValue = value + 1;
-                countValue.setText(asString(incrementedValue));
-            });
-            countValue.setEditable(false);
-            countValue.setPrefWidth(50);
-
-            // create and add containers.
-            HBox box = new HBox(10, countButton, countValue);
-            box.setPadding(new Insets(10));
-            box.setAlignment(Pos.CENTER);
-            getChildren().add(box);
-        }
-    }
-
-    private static int asInteger(String value) { return Integer.parseInt(value); }
-    private static String asString(int value) { return value + ""; }
 
     @Override
     public void start(Stage stage) throws Exception {
